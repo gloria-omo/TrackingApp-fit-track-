c2a9a693cf5623ce53a616bfd30816f5f1f50ebf
unknown <gloriaakubor7@gmail.com>
2024-02-23 13:53:12 +0100

    sign-up update

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,7 +5,7 @@ const validation = async(req,res,next)=>{
         companyName:joi.string().min(3).max(50),
         email:joi.string().email().required(),
         phoneNumber:joi.string().pattern(new RegExp('^[0-9]')).min(11).max(11),
-        password:joi.string().pattern(new RegExp ('^[a-zA-Z0-9]{3,30}$')),
+        password:joi.string().min(6).max(20).pattern(new RegExp ('^[a-zA-Z0-9]{3,30}$')),
         confirmPassword:joi.string().pattern(new RegExp ('^[a-zA-Z0-9]{3,30}$'))
     })
     next()
