38c6115866f3be99eea1cb3630864b3c32be239b
unknown <gloriaakubor7@gmail.com>
2024-03-03 20:08:44 +0100

    add getAllUsersWithThreeMonthPlan router

--- routers/clientRouter.js
+++ routers/clientRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createClient, getAll, getOne, calculateRemainingDays, createPlan, batchOnboard, deleteUser, cronMiddleware, getAllActiveMember, getAllNonActiveMember,  } = require("../controllers/clientController");
+const { createClient, getAll, getOne, calculateRemainingDays, createPlan, batchOnboard, deleteUser, cronMiddleware, getAllActiveMember, getAllNonActiveMember, getAllUsersWithOneMonthPlan, getAllUsersWithTwoMonthPlan, getAllUsersWithThreeMonthPlan,  } = require("../controllers/clientController");
 const { checkTrialPeriod } = require("../controllers/userController");
 const authenticate = require("../middlewares/authentication");
 
@@ -10,6 +10,9 @@ router.put("/addPlan/:id",authenticate,checkTrialPeriod,createPlan);
 router.get("/getAllMember",authenticate,getAll);
 router.get("/getAllActiveMember",authenticate,getAllActiveMember);
 router.get("/getAllNonActiveMember",authenticate,getAllNonActiveMember);
+router.get("/getAllOneMonthPlan",authenticate,getAllUsersWithOneMonthPlan);
+router.get("/getAllThreeMonthPlan",authenticate,getAllUsersWithThreeMonthPlan);
+router.get("/getAllTwoMonthPlan",authenticate,getAllUsersWithTwoMonthPlan);
 router.get("/getoneMember",authenticate,checkTrialPeriod,getOne);
 router.post("/createBulkUser",authenticate,checkTrialPeriod,batchOnboard);
 router.get("/daysremainingCron/:id",authenticate,checkTrialPeriod,cronMiddleware);
